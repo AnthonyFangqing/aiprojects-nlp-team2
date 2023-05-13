3e804e419dade406090e0680517be592dc2194ef
Anthony Yu <anthony.fangqing@gmail.com>
2023-05-12 18:56:56 -0700

    This should be the important stuff

--- main.py
+++ main.py
@@ -1,34 +1,54 @@
 import os
-
+import torch
 import constants
 from data.StartingDataset import StartingDataset
 from networks.StartingNetwork import StartingNetwork
 from train_functions.starting_train import starting_train
-
+from torch.utils.data import random_split
 
 def main():
+    """
+    Reads in data and 
+    """
     # Get command line arguments
-    hyperparameters = {"epochs": constants.EPOCHS, "batch_size": constants.BATCH_SIZE}
+    hyperparameters = {"epochs": constants.EPOCHS, "batch_size": constants.BATCH_SIZE, "train_val_split": constants.TRAIN_VAL_SPLIT}
 
     # TODO: Add GPU support. This line of code might be helpful.
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Device: ", device)
 
     print("Epochs:", constants.EPOCHS)
     print("Batch size:", constants.BATCH_SIZE)
 
     # Initalize dataset and model. Then train the model!
     data_path = "train.csv" #TODO: make sure you have train.csv downloaded in your project! this assumes it is in the project's root directory (ie the same directory as main) but you can change this as you please
+    # 1306122 rows -- they gave us 12122002 by default
+    # qid, question_text, target
+    # test_csv has no targets
 
-    train_dataset = StartingDataset(data_path)
-    val_dataset = StartingDataset(data_path)
-    model = StartingNetwork()
-    starting_train(
-        train_dataset=train_dataset,
-        val_dataset=val_dataset,
-        model=model,
-        hyperparameters=hyperparameters,
-        n_eval=constants.N_EVAL,
-    )
+    whole_dataset = StartingDataset(data_path)
+
+    # define the sizes of your training and validation sets
+    train_size = int(constants.TRAIN_VAL_SPLIT * len(whole_dataset))
+    val_size = len(whole_dataset) - train_size
+
+    # use random_split to split the dataset into non-overlapping
+    # training and validation sets
+    train_dataset, val_dataset = random_split(whole_dataset, [train_size, val_size])
+    # why do you need two identical Dataset objects?
+    # should be changed in some way so that val_dataset only does validation
+
+    #model = load_model()
+    #predict_sentiment(model, whole_dataset, "this is a true statement")
+
+
+    model = StartingNetwork(len(whole_dataset.token2idx))
+    starting_train(train_dataset=train_dataset, val_dataset=val_dataset, model=model, hyperparameters=hyperparameters, n_eval=constants.N_EVAL,) # call the training function from starting_train.py
+    # hyperparameters from constants.py
+    # can customize model
+    #return whole_dataset
+    return model
+
 
 if __name__ == "__main__":
-    main()
+    finished_model = main()
--- train_functions/starting_train.py
+++ train_functions/starting_train.py
@@ -30,31 +30,53 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval):
     # Initalize optimizer (for gradient descent) and loss function
     optimizer = optim.Adam(model.parameters())
     loss_fn = nn.CrossEntropyLoss()
-
+    train_losses = []
     step = 0
     for epoch in range(epochs):
         print(f"Epoch {epoch + 1} of {epochs}")
-
+        losses = []
         # Loop over each batch in the dataset
         for batch in tqdm(train_loader):
-            # TODO: Forward propagate
+            # Zero the gradients
+            model.zero_grad()
 
-            # TODO: Backpropagation and gradient descent
+            # Forward propagate
+            inputs, labels = batch
+            outputs = model(inputs)
+            loss = loss_fn(outputs, labels)
+
+            # Backpropagation and gradient descent
+            loss.backward()
+            optimizer.step()
+
+            losses.append(loss.item())
 
             # Periodically evaluate our model + log to Tensorboard
-            if step % n_eval == 0:
-                # TODO:
-                # Compute training loss and accuracy.
-                # Log the results to Tensorboard.
+            if step % n_eval == -1:
+                # Compute training loss and accuracy
+                with torch.no_grad():
+                    train_loss, train_acc = evaluate(train_loader, model, loss_fn)
+
+                 # Log training results to console
+                print(f"Step {step}, Training Loss: {train_loss:.4f}, Training Accuracy: {train_acc:.4f}")
+
+                # Compute validation loss and accuracy
+                # with torch.no_grad():
+                    # val_loss, val_acc = evaluate(val_loader, model, loss_fn)
+
+                # Log validation results to console
+                # print(f"Step {step}, Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_acc:.4f}")
 
                 # TODO:
                 # Compute validation loss and accuracy.
                 # Log the results to Tensorboard. 
                 # Don't forget to turn off gradient calculations!
-                evaluate(val_loader, model, loss_fn)
+                # evaluate(val_loader, model, loss_fn)
 
             step += 1
-
+        epoch_loss = sum(losses) / step
+        train_losses.append(epoch_loss)
+        tqdm.write(f'Epoch #{epoch + 1}\tTrain Loss: {epoch_loss:.3f}')
         print()
 
 
@@ -75,10 +97,28 @@ def compute_accuracy(outputs, labels):
     return n_correct / n_total
 
 
-def evaluate(val_loader, model, loss_fn):
+def evaluate(loader, model, loss_fn):
     """
     Computes the loss and accuracy of a model on the validation dataset.
-
-    TODO!
     """
-    pass
+    model.eval()  # Put the model in evaluation mode
+    total_loss, total_acc = 0, 0
+    n_examples = 0
+
+    with torch.no_grad():  # Turn off gradient calculations
+        for batch in loader:
+            inputs, labels = batch
+            outputs = model(inputs)
+            loss = loss_fn(outputs, labels)
+            acc = compute_accuracy(outputs, labels)
+
+            total_loss += loss.item() * len(inputs)
+            total_acc += acc * len(inputs)
+            n_examples += len(inputs)
+
+    avg_loss = total_loss / n_examples
+    avg_acc = total_acc / n_examples
+
+    print(f"Validation loss: {avg_loss:.4f}, accuracy: {avg_acc:.4f}")
+
+    model.train()  # Put the model back in training mode
